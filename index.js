64182fb14fd055deb3ca9ad1597c4ff4689b9539
John Garrett <jacgarrett18@gmail.com>
2021-12-22 12:52:28 -0500

    add key coloring on depress

--- index.js
+++ index.js
@@ -8,6 +8,46 @@ if (navigator.requestMIDIAccess) {
   console.warn("No MIDI support in your browser")
 }
 
+function midiNumberToNote(number) {
+    function getOctave(number) {
+        console.warn(number / (number % 12))
+    }
+    /*
+      {
+        note: '',
+        isSharp: '',
+        octave: #,
+        velocity: #,
+      }
+     */
+    console.log(number / (number % 12))
+    switch (number % 12) {
+        case 0:
+            return 'C'
+        case 1:
+            return 'C#/Db'
+        case 2:
+            return 'D'
+        case 3:
+            return 'D#/Eb'
+        case 4:
+            return 'E'
+        case 5: 
+            return 'F'
+        case 6:
+            return 'F#/Gb'
+        case 7:
+            return 'G'
+        case 8: 
+            return 'G#/Ab'
+        case 9:
+            return 'A'
+        case 10:
+            return 'A#/Bb'
+        case 11:
+            return 'B'
+    }
+}
 
 function onMIDISuccess(midiAccess) {
         for (var input of midiAccess.inputs.values()) {
@@ -18,25 +58,28 @@ function onMIDISuccess(midiAccess) {
 var dataList = document.querySelector('#midi-data ul')
 function getMIDIMessage(midiMessage) {
     console.log(midiMessage.data[1], midiMessage.data[2]);
-    colorBox(midiMessage.data[1])
-    //var newItem = document.createElement('li');
-    //newItem.appendChild(document.createTextNode(midiMessage.data));
-    //dataList.appendChild(newItem);
+    let note = midiNumberToNote(midiMessage.data[1])
+    colorKey(note)
+    displayNote(note)
 }
 
-function colorBox(index) {
-    let box = document.querySelector('#midi-boxes-'+index)
-    if (!box) {
+function colorKey(note) {
+    let key = document.getElementById('key-'+note)
+    if (!key) {
         console.log('no box found')
         let newBox = document.createElement("button");
         newBox.innerHTML = index;
         newBox.setAttribute('id', 'midi-boxes-'+index)
         document.getElementById("midi-boxes").appendChild(newBox)
     }
-    $("#midi-boxes-"+index).animate({
-        opacity: 0.5
-    },
-    300)
+    key.classList.add('selected')
+    setInterval(function() {
+        key.classList.remove('selected')
+    }, 500)
+}
+
+function displayNote(note) {
+    document.getElementById('current-note').textContent = note;
 }
 
 function onMIDIFailure() {
